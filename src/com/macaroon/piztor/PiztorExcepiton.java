ac7633d8149a28af288ac0b850850cef9b13c151
sjtufs <sjtu_fs@outlook.com>
2013-08-25 15:45:55 +0800

    This is alpha

---
+++ src/com/macaroon/piztor/PiztorExcepiton.java
@@ -0,0 +1,10 @@
+package com.macaroon.piztor;
+
+class PiztorExcepiton extends Exception{
+	private static final long serialVersionUID = -7147530695009854988L;
+
+}
+
+class ClassCannotFind extends PiztorExcepiton {
+	//private class 
+}
